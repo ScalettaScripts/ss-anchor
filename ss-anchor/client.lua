be1570c5248d8eb8c3fbb861a973c55b4206617e
Ari Scalleta <fivemplayer478@gmail.com>
2024-10-25 01:28:46 +1100

    Added Basic Sounds

--- ss-anchor/client.lua
+++ ss-anchor/client.lua
@@ -7,33 +7,41 @@ RegisterCommand("anchor", function(source)
     if IsPedInAnyBoat(ped) then
         local boat = GetVehiclePedIsIn(ped)
         if GetPedInVehicleSeat(boat, -1) == ped then
-            if GetEntitySpeed(ped) <= config.speedToDeploy / 3.6 then -- KM/H
+            if GetEntitySpeed(boat) <= config.speedToDeploy / 3.6 then 
                 if IsBoatAnchoredAndFrozen(boat) then
                     SetBoatAnchor(boat, false)
                     SetBoatFrozenWhenAnchored(boat, false)
                     SetForcedBoatLocationWhenAnchored(boat, false)
-                    SendNotification(config.Locales[currentLang].raisedText)
+                    SendNotification(config.Locales[currentLang].raisedText, "ANCHOR_RAISE_SOUND") 
                 else
                     if CanAnchorBoatHere(boat) then
                         SetBoatAnchor(boat, true)
                         SetBoatFrozenWhenAnchored(boat, true)
                         SetForcedBoatLocationWhenAnchored(boat, true)
-                        SendNotification(config.Locales[currentLang].loweredText)
+                        SendNotification(config.Locales[currentLang].loweredText, "ANCHOR_DROP_SOUND") 
                     else
-                        SendNotification(config.Locales[currentLang].notInWaterText)
+                        SendNotification(config.Locales[currentLang].notInWaterText, "ERROR_SOUND") 
                     end
                 end
             else
-                SendNotification(config.Locales[currentLang].tooFastText)
+                SendNotification(config.Locales[currentLang].tooFastText, "ERROR_SOUND") 
             end
         end
     end
 end, false)
 
-function SendNotification(message)
+function SendNotification(message, sound)
     BeginTextCommandThefeedPost("STRING")
     AddTextComponentSubstringPlayerName(message)
     EndTextCommandThefeedPostTicker(false, true)
+
+    if sound == "ANCHOR_RAISE_SOUND" then
+        PlaySoundFromEntity(-1, "PICK_UP", PlayerPedId(), "HUD_FRONTEND_DEFAULT_SOUNDSET", 0, 0)
+    elseif sound == "ANCHOR_DROP_SOUND" then
+        PlaySoundFromEntity(-1, "Dropped", PlayerPedId(), "HUD_FRONTEND_MP_COLLECTABLE_SOUNDS", 0, 0)
+    elseif sound == "ERROR_SOUND" then
+        PlaySoundFromEntity(-1, "Pin_Bad", PlayerPedId(), "DLC_HEIST_BIOLAB_PREP_HACKING_SOUNDS", 0, 0)
+    end
 end
 
 if config.Locales[currentLang].boatAnchoredText then
@@ -43,7 +51,7 @@ if config.Locales[currentLang].boatAnchoredText then
             local ped = PlayerPedId()
             local veh = GetVehiclePedIsEntering(ped)
             if IsBoatAnchoredAndFrozen(veh) then
-                SendNotification(config.Locales[currentLang].boatAnchoredText)
+                SendNotification(config.Locales[currentLang].boatAnchoredText, "ANCHOR_RAISE_SOUND")
             end
         end
     end)
