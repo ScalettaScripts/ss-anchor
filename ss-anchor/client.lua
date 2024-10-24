f21df14b21798114f66a5f292d42d30541e98db0
Ari Scalleta <fivemplayer478@gmail.com>
2024-10-25 01:06:33 +1100

    Locale Support

--- ss-anchor/client.lua
+++ ss-anchor/client.lua
@@ -1,5 +1,6 @@
 RegisterKeyMapping("anchor", "Anchor Boat", "keyboard", "e")
 local config = Config
+local currentLang = config.defaultLang
 
 RegisterCommand("anchor", function(source)
     local ped = PlayerPedId()
@@ -11,19 +12,19 @@ RegisterCommand("anchor", function(source)
                     SetBoatAnchor(boat, false)
                     SetBoatFrozenWhenAnchored(boat, false)
                     SetForcedBoatLocationWhenAnchored(boat, false)
-                    SendNotification(config.raisedText)
+                    SendNotification(config.Locales[currentLang].raisedText)
                 else
                     if CanAnchorBoatHere(boat) then
                         SetBoatAnchor(boat, true)
                         SetBoatFrozenWhenAnchored(boat, true)
                         SetForcedBoatLocationWhenAnchored(boat, true)
-                        SendNotification(config.loweredText)
+                        SendNotification(config.Locales[currentLang].loweredText)
                     else
-                        SendNotification(config.notInWaterText)
+                        SendNotification(config.Locales[currentLang].notInWaterText)
                     end
                 end
             else
-                SendNotification(config.tooFastText)
+                SendNotification(config.Locales[currentLang].tooFastText)
             end
         end
     end
@@ -35,14 +36,14 @@ function SendNotification(message)
     EndTextCommandThefeedPostTicker(false, true)
 end
 
-if config.boatAnchoredText then
+if config.Locales[currentLang].boatAnchoredText then
     CreateThread(function()
         while true do
             Wait(500)
             local ped = PlayerPedId()
             local veh = GetVehiclePedIsEntering(ped)
             if IsBoatAnchoredAndFrozen(veh) then
-                SendNotification(config.boatAnchoredText)
+                SendNotification(config.Locales[currentLang].boatAnchoredText)
             end
         end
     end)
